4a3e0552dd5748bc24e5f4fe24475458e389085b
Arsen <arsenvakhidov@gmail.com>
2024-12-28 16:48:21 +0200

    Added global server

--- src/Helpers/Connect.tsx
+++ src/Helpers/Connect.tsx
@@ -1,7 +1,8 @@
 import React from "react"
 import axios from "axios"
 
-const server = "http://127.0.0.1:5000"
+//const server = "http://127.0.0.1:5000"
+const server = "https://dilib-host.loca.lt"
 
 export const Connect = {
     async saveHTML(formData) {
