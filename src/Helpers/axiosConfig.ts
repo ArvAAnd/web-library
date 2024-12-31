a2931cb511193bb27d853a904634814ccbda220c
Arsen <arsenvakhidov@gmail.com>
2024-12-31 16:20:59 +0200

    changed server

--- src/Helpers/axiosConfig.ts
+++ src/Helpers/axiosConfig.ts
@@ -7,8 +7,8 @@ export const getConfig = () => {
 };
 
 export const axiosConfig = axios.create({
-    baseURL:"http://127.0.0.1:5000/", //offline fast server (on pc)
+    //baseURL:"http://127.0.0.1:5000/", //offline fast server (on pc)
     //baseURL:"https://dilib-host.loca.lt/", //online slow server (on pc)
-    //baseURL: "https://book-server-yec5.onrender.com/", //online fast server
+    baseURL: "https://book-server-yec5.onrender.com/", //online fast server
     ...getConfig(),
 });
